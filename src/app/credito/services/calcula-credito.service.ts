00e4d590113cad7b2cdf2e845dc57adf49584151
MattheusCosta <matheuscosta1921@gmail.com>
2019-10-18 14:59:15 -0300

    serviço adicionado

--- src/app/credito/services/calcula-credito.service.ts
+++ src/app/credito/services/calcula-credito.service.ts
@@ -6,4 +6,11 @@ import { Injectable } from '@angular/core';
 export class CalculaCreditoService {
 
   constructor() { }
+
+  depositar(valorSaldo: number, valorDeposito: number):number{
+    return valorSaldo+=valorDeposito;
+  }
+  saque(valorSaldo: number, valorSaque: number):number{
+    return valorSaldo+=valorSaque;
+  }
 }
